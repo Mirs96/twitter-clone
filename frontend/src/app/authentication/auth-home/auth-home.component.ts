cfce72ae11888c9dc4dafeafa1b9a0be14230bd1
Miryam Massa <miryammassa@gmail.com>
2025-03-03 14:49:45 +0100

    Add navbar and create some empty components

---
+++ frontend/src/app/authentication/auth-home/auth-home.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-auth-home',
+  imports: [],
+  templateUrl: './auth-home.component.html',
+  styleUrl: './auth-home.component.css'
+})
+export class AuthHomeComponent {
+
+}
